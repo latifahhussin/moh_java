bd4c9a43f347c0cfd9bae9f50a05a40445116e89
latifah <latifahhsn@gmail.com>
2019-03-05 16:06:08 +0800

    commit 3rd

--- javatot/src/chapter22/Film.java
+++ javatot/src/chapter22/Film.java
@@ -14,7 +14,16 @@ public class Film {
        // try ..catch = exception/error handling
        // code berada dalam try, catch bila berlaku error
        try{
-          con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila?useLegacyDatetimeCode=false&serverTimezone=America/New_York", "root", "");
+          con =  DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila?useLegacyDatetimeCode=false&serverTimezone=America/New_York", "root", "");         
+       stmt = con.createStatement();
+       String sql = "SELECT * FROM film";
+       rs = stmt.executeQuery(sql);
+       while(rs.next()){
+           int id = rs.getInt("film_id");
+           System.out.print(id + "\t");
+           System.out.print(rs.getString("title") + "\t");
+           System.out.println(rs.getString("description"));
+       }
        } catch(Exception e){
            System.out.println("Berlaku Error");
            System.out.println(e.getMessage());
